adf660bce0a3a1a51aa42c921d190272efa4afe6
akshatmittal61 <akshatmittal2506@gmail.com>
2022-02-03 13:28:42 +0530

    added routes

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import { config } from "dotenv";
+import apiUsers from "./Routes/API/users.js";
+import apiAuth from "./Routes/API/auth.js";
+import apiProfile from "./Routes/API/profile.js";
+import apiPosts from "./Routes/API/posts.js";
 const app = express();
 
 config();
@@ -23,6 +27,11 @@ connectDB();
 app.get("/", (req, res) => {
 	res.send("Hello world");
 });
+
+app.use("/api/users", apiUsers);
+app.use("/api/auth", apiAuth);
+app.use("/api/profile", apiProfile);
+app.use("/api/posts", apiPosts);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started at port ${PORT}`));
