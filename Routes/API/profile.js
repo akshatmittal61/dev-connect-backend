082cb603d517a24c620ac05acb81ed5ca1c1df27
akshatmittal61 <akshatmittal2506@gmail.com>
2022-02-04 13:36:54 +0530

    added get profile route

--- Routes/API/profile.js
+++ Routes/API/profile.js
@@ -1,8 +1,25 @@
 import express from "express";
+import auth from "../../middleware/auth.js";
+import User from "../../models/User.js";
+import Profile from "../../models/Profile.js";
 const router = express.Router();
 
-router.get("/", (req, res) => {
-	res.send("Profile route");
+router.get("/me", auth, async (req, res) => {
+	try {
+		const profile = await Profile.findOne({ user: req.user.id }).populate(
+			"user",
+			["name", "avatar"]
+		);
+		if (!profile) {
+			return res
+				.status(400)
+				.json({ message: "There is no profile for this user" });
+		}
+		res.json(profile);
+	} catch (err) {
+		console.log(err.message);
+		res.status(500).send("server error");
+	}
 });
 
 export default router;
