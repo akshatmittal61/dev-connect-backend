79d06cc73fb707fc1a841505d772a4c1bdde4f63
akshatmittal61 <akshatmittal2506@gmail.com>
2022-02-14 01:49:32 +0530

    add experince

--- Routes/API/profile.js
+++ Routes/API/profile.js
@@ -136,4 +136,43 @@ router.delete("/", auth, async (req, res) => {
 	}
 });
 
+router.put(
+	"/experience",
+	[
+		auth,
+		[
+			check("title", "Title is required").not().isEmpty(),
+			check("company", "Company is required").not().isEmpty(),
+			check("from", "From date is required").not().isEmpty(),
+		],
+	],
+	async (req, res) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ error: errors.array() });
+		}
+		const { title, company, location, from, to, current, description } =
+			req.body;
+		const newExp = {
+			title,
+			company,
+			location,
+			from,
+			to,
+			current,
+			description,
+		};
+		try {
+			const profile = await Profile.findOne({ user: req.user.id });
+			profile.experince.unshift(newExp);
+			await profile.save();
+			res.json(profile);
+			res.send("Hello");
+		} catch (err) {
+			console.log(err.message);
+			res.status(500).send("Server Error");
+		}
+	}
+);
+
 export default router;
