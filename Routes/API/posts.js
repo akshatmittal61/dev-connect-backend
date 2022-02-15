5ed74801f0ae10c57239059c1ef49f410cf3f36c
akshatmittal61 <akshatmittal2506@gmail.com>
2022-02-15 15:41:36 +0530

    like and unlike post

--- Routes/API/posts.js
+++ Routes/API/posts.js
@@ -70,4 +70,47 @@ router.delete("/:id", auth, async (req, res) => {
 	}
 });
 
+router.put("/like/:id", auth, async (req, res) => {
+	try {
+		const post = await Post.findById(req.params.id);
+		if (!post) return res.status(404).json({ message: "Post Not found" });
+		if (
+			post.likes.filter((like) => like.user.toString() === req.user.id)
+				.length > 0
+		)
+			return res.status(400).json({ message: "Post already liked" });
+		post.likes.unshift({ user: req.user.id });
+		await post.save();
+		res.json(post.likes);
+	} catch (err) {
+		console.error(err);
+		if (err.kind === "ObjectId")
+			return res.status(404).json({ message: "Post Not found" });
+		res.status(500).send("Server error");
+	}
+});
+
+router.put("/unlike/:id", auth, async (req, res) => {
+	try {
+		const post = await Post.findById(req.params.id);
+		if (!post) return res.status(404).json({ message: "Post Not found" });
+		if (
+			post.likes.filter((like) => like.user.toString() === req.user.id)
+				.length === 0
+		)
+			return res.status(400).json({ message: "Post not liked yet" });
+		const remIndex = post.likes
+			.map((like) => like.user.toString())
+			.indexOf(req.user.id);
+		post.likes.splice(remIndex, 1);
+		await post.save();
+		res.json(post.likes);
+	} catch (err) {
+		console.error(err);
+		if (err.kind === "ObjectId")
+			return res.status(404).json({ message: "Post Not found" });
+		res.status(500).send("Server error");
+	}
+});
+
 export default router;
